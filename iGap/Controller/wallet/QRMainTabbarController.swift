694bb7b9334e9a2afa4e082828d5daa3c77cd5e3
SaeedMozaffari <mozaffari.saeed10@gmail.com>
2019-05-28 18:06:58 +0430

    Fix - Wallet Scanner String

--- iGap/Controller/wallet/QRMainTabbarController.swift
+++ iGap/Controller/wallet/QRMainTabbarController.swift
@@ -21,31 +21,26 @@ class QRMainTabbarController: UITabBarController {
         override func viewDidLoad() {
             super.viewDidLoad()
             self.tabBar.barTintColor = UIColor.iGapBars()
-            tabBar.items![0].title = "MY_QR".localizedNew
-            tabBar.items![1].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
-
+            tabBar.items![0].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
+            tabBar.items![1].title = "MY_QR".localizedNew
         }
         
         override func viewWillAppear(_ animated: Bool) {
             selectedItemTitleMustbeBold()
-            tabBar.items![0].title = "MY_QR".localizedNew
-            tabBar.items![1].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
-
+            tabBar.items![0].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
+            tabBar.items![1].title = "MY_QR".localizedNew
         }
         
         override func viewDidAppear(_ animated: Bool) {
             selectedItemTitleMustbeBold()
-            tabBar.items![0].title = "MY_QR".localizedNew
-            tabBar.items![1].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
-
+            tabBar.items![0].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
+            tabBar.items![1].title = "MY_QR".localizedNew
         }
-        
         
         override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
             selectedItemTitleMustbeBold()
-            tabBar.items![0].title = "MY_QR".localizedNew
-            tabBar.items![1].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
-
+            tabBar.items![0].title = "SETTING_PAGE_QRCODE_SCANNER".localizedNew
+            tabBar.items![1].title = "MY_QR".localizedNew
         }
         
         func selectedItemTitleMustbeBold(){
