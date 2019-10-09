05c15ecf112a60d1c2b3f75ee4f0617e6bec47a0
Saeed Mozaffari <mozaffari.saeed10@gmail.com>
2019-10-09 18:18:24 +0330

    Improvement - Update TabBar View

--- iGap/Controller/Custom Master Controllers/Tabbar/IGTabBarController.swift
+++ iGap/Controller/Custom Master Controllers/Tabbar/IGTabBarController.swift
@@ -65,7 +65,6 @@ class IGTabBarController: UITabBarController {
         for item in tabBar.items! {
             if #available(iOS 10.0, *) {
                 item.badgeColor = UIColor.unreadLable()
-//                item.badgeValue = "2".inLocalizedLanguage()
                 item.badgeValue = item.badgeValue?.inLocalizedLanguage()
                 item.setBadgeTextAttributes([
                     NSAttributedString.Key.foregroundColor: UIColor.white,
@@ -81,42 +80,30 @@ class IGTabBarController: UITabBarController {
                 item.badgeColor = UIColor.unreadLable()
             }
         }
-        let navigationControllerr = self.navigationController as! IGNavigationController
+        let navigationController = self.navigationController as! IGNavigationController
         
         switch item {
         case .Contact:
-            navigationControllerr.navigationBar.isHidden = false
-            //            navigationControllerr.addSearchBar(state: "True")
-            let navigationItem = self.navigationItem as! IGNavigationItem
-            
+            navigationController.navigationBar.isHidden = false
             currentTabIndex = TabBarTab.Contact.rawValue
             break
         case .Call:
-            navigationControllerr.navigationBar.isHidden = false
-            //            navigationControllerr.addSearchBar(state: "False")
-            let navigationItem = self.navigationItem as! IGNavigationItem
-            
+            navigationController.navigationBar.isHidden = false
             currentTabIndex = TabBarTab.Call.rawValue
             
             break
         case .Recent:
-            navigationControllerr.navigationBar.isHidden = false
-            
+            navigationController.navigationBar.isHidden = false
             currentTabIndex = TabBarTab.Recent.rawValue
             
             break
         case .Dashboard:
-            navigationControllerr.navigationBar.isHidden = false
-            //            navigationControllerr.addSearchBar(state: "False")
-            let navigationItem = self.navigationItem as! IGNavigationItem
-            
+            navigationController.navigationBar.isHidden = false
             currentTabIndex = TabBarTab.Dashboard.rawValue
             
             break
         case .Profile:
-            let navigationItem = self.navigationItem as! IGNavigationItem
             currentTabIndex = TabBarTab.Profile.rawValue
-            
             break
         }
     }
@@ -127,41 +114,40 @@ class IGTabBarController: UITabBarController {
         myTabBarItem1.selectedImage = UIImage(named: "ig-Phone-Book-on_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem1.title = "TAB_PHONEBOOK".localizedNew
         myTabBarItem1.tag = 0
-        myTabBarItem1.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        
+        myTabBarItem1.imageInsets = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
+        myTabBarItem1.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
         
         let myTabBarItem2 = (self.tabBar.items?[1])! as UITabBarItem
         myTabBarItem2.image = UIImage(named: "ig-Call-List_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem2.selectedImage = UIImage(named: "ig-Call-List-on_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem2.title = "TAB_CALL_LIST".localizedNew
         myTabBarItem2.tag = 1
-        myTabBarItem2.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        
+        myTabBarItem2.imageInsets = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
+        myTabBarItem2.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
         
         let myTabBarItem3 = (self.tabBar.items?[2])! as UITabBarItem
         myTabBarItem3.image = UIImage(named: "ig-Room-List-Off_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem3.selectedImage = UIImage(named: "ig-Room-List-on_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem3.title = "TAB_CHAT".localizedNew
         myTabBarItem3.tag = 2
-        
-        myTabBarItem3.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        myTabBarItem3.imageInsets = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
+        myTabBarItem3.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
         
         let myTabBarItem4 = (self.tabBar.items?[3])! as UITabBarItem
         myTabBarItem4.image = UIImage(named: "ig-Dashboard-off_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem4.selectedImage = UIImage(named: "ig-Discovery-on_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem4.title = "TAB_DISCOVERY".localizedNew
         myTabBarItem4.tag = 3
-        
-        myTabBarItem4.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        myTabBarItem4.imageInsets = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
+        myTabBarItem4.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
         
         let myTabBarItem5 = (self.tabBar.items?[4])! as UITabBarItem
         myTabBarItem5.image = UIImage(named: "ig-Settings-off_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem5.selectedImage = UIImage(named: "ig-Settings-on_25")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
         myTabBarItem5.title = "TAB_PROFILE".localizedNew
         myTabBarItem5.tag = 4
-        
-        myTabBarItem5.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
-        
+        myTabBarItem5.imageInsets = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
+        myTabBarItem5.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: -10)
     }
 
     
