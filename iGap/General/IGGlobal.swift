994ed8cbcda9f571c4ecc1a1cdc7ce3a478d4ff2
benyamin mokhtarpour <b.mokhtarpour@kianiranian.com>
2020-05-26 14:10:05 +0430

    #KUKNOS - QR code Generator for Kuknos

--- iGap/General/IGGlobal.swift
+++ iGap/General/IGGlobal.swift
@@ -155,6 +155,23 @@ class IGGlobal {
         var total: Int = 0
         var count: Int = 0
     }
+    internal static func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+
+        return nil
+    }
+
+    let image = generateQRCode(from: "Hacking with Swift is the best iOS coding tutorial I've ever read!")
+
     internal static func getDate(from: String) -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
